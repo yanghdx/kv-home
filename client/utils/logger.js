ea133769fcdfefd71778be24d1c4ce5f28f8695a
yanghdx <yanghdx@163.com>
2017-01-06 23:16:20 +0800

    add video.js

---
+++ client/utils/logger.js
@@ -0,0 +1,17 @@
+function info(msg) {
+    console.info(msg)
+}
+
+function error(msg) {
+    console.error(msg)
+}
+
+function warn(msg) {
+    console.warn(msg)
+}
+
+export default {
+    info,
+    warn,
+    error
+}
--- client/views/videos/movies.vue
+++ client/views/videos/movies.vue
@@ -1,3 +1,23 @@
 <template>
-    <div>movies</div>
+    <div>
+        <el-row>
+            <el-col :span="20" :offset="2">
+                <div class="grid-content bg-purple-dark">
+                    <video id="my-video" class="video-js" controls preload="auto" style="width:100%;height:600px;">
+                        <source src="1.mp4" type='video/mp4'>
+                    </video>
+                </div>
+            </el-col>
+        </el-row>
+    </div>
 </template>
+<script>
+    import 'video.js'
+    export default {
+        data() {
+            return {
+                name: '1.mp4'
+            }
+        }
+    }
+</script>
